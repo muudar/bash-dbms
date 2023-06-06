0b3b4390dd8a126bc82310a08f7718f348d8cd38
ahmedgamaleldinn <135102799+ahmedgamaleldinn@users.noreply.github.com>
2023-06-06 20:28:15 +0300

    Created insert to table function

--- main.sh
+++ main.sh
@@ -119,7 +119,61 @@ read tableName
 }
 
 function insertToTable(){
-   
+   echo -e "Table Name: \c"
+  read tableName
+  if ! [[ -f $tableName ]]; then
+    echo "Table $tableName doesn't exist!"
+    printDBmenu
+  elif [[ ! $tableName =~ ^[a-zA-Z]+$ ]]; then
+  echo "Table name can only contain letters!"
+  printDBmenu
+  fi
+  colsNum=`awk 'END{print NR}' .$tableName`
+  sep="|"
+  rSep="\n"
+  for (( i = 2; i <= $colsNum; i++ )); do
+    colName=$(awk 'BEGIN{FS="|"}{ if(NR=='$i') print $1}' .$tableName)
+    colType=$( awk 'BEGIN{FS="|"}{if(NR=='$i') print $2}' .$tableName)
+    colKey=$( awk 'BEGIN{FS="|"}{if(NR=='$i') print $3}' .$tableName)
+    echo -e "$colName ($colType) = \c"
+    read data
+
+    # Validate Input
+    if [[ $colType == "int" ]]; then
+      while ! [[ $data =~ ^[0-9]*$ ]]; do
+        echo -e "invalid DataType !!"
+        echo -e "$colName ($colType) = \c"
+        read data
+      done
+    fi
+     if [[ $colKey == "PK" ]]; then
+      while [[ true ]]; do
+        if [[ $data =~ ^[`awk 'BEGIN{FS="|" ; ORS=" "}{if(NR != 1)print $(('$i'-1))}' $tableName`]$ ]]; then
+          echo -e "invalid input for Primary Key !!"
+        else
+          break;
+        fi
+        echo -e "$colName ($colType) = \c"
+        read data
+      done
+    fi
+
+    #Set row
+    if [[ $i == $colsNum ]]; then
+      row=$row$data$rSep
+    else
+      row=$row$data$sep
+    fi
+  done
+  echo -e $row"\c" >> $tableName
+  if [[ $? == 0 ]]
+  then
+    echo "Data Inserted Successfully"
+  else
+    echo "Error Inserting Data into Table $tableName"
+  fi
+  row=""
+  printDBmenu
 }
 function printDBmenu() {
     echo -e "\nSelect an option:"
