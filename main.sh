0a9d1a9e98a91afb9ddc9db36fe2b71968757339
YoussefNassar <youssefnassar977@gmail.com>
2023-06-06 19:15:46 +0300

    creat tabel function

--- main.sh
+++ main.sh
@@ -42,7 +42,66 @@ function removeDB() {
 }
 
 function createTable(){
+echo -e "Table Name: \c"
+  read tableName
+  if [[ -f $tableName ]]; then
+    echo "Table already exists"
+    createTable
+    return
+  fi
+  echo -e "Number of Columns: \c"
+  read colsNum
+  if ! [[ $colsNum =~ ^[0-9]+$ ]]; then
+        echo "Invalid input. Please enter a number."
+        createTable
+        return
+    elif (( $colsNum < 1 || $colsNum > 20 )); then
+        echo "Invalid Input. Please enter a number between 1 and 20."
+        createTable
+        return
+    fi
+  counter=1
+  sep="|"
+  rSep="\n"
+  metaData="Field"$sep"Type"$sep"key"
+  while [ $counter -le $colsNum ]
+  do
+    echo -e "Name of Column No.$counter: \c"
+    read colName
 
+    echo -e "Type of Column $colName: "
+    select var in "int" "str"
+    do
+      case $var in
+        int ) colType="int";break;;
+        str ) colType="str";break;;
+        * ) echo "Wrong Choice" ;;
+      esac
+    done
+    if [[ "$counter" -eq 1 ]]; then
+        metaData+=$rSep$colName$sep$colType$sep$"PK";
+    else
+        metaData+=$rSep$colName$sep$colType$sep""
+    fi
+    if [[ $counter == $colsNum ]]; then
+      temp=$temp$colName
+    else
+      temp=$temp$colName$sep
+    fi
+    ((counter++))
+  done
+  touch .$tableName
+  echo -e $metaData  >> .$tableName
+  touch $tableName
+  echo -e $temp >> $tableName
+  if [[ $? == 0 ]]
+  then
+    echo "Table Created Successfully"
+    echo $tableName
+  else
+    echo "Error Creating Table $tableName"
+    
+  fi
 }
 function printDBmenu() {
     echo -e "\nSelect an option:"
